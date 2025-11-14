b1e81c7b14d4806faa36664075397a1597e4e445
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-11-14 15:31:10 -0500

    Update WeatherManager.cs

--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class WeatherManager : MonoBehaviour
 {
-    private const string xmlApi = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=166e0498ec70931f9ce60429eb4fe361";
+    private string APIKey = "166e0498ec70931f9ce60429eb4fe361";
+
+    private Location[] Locations = 
+    {
+        new Location("Orlando", "OrlandoCode", "CountryCode"),
+        new Location("Los Angeles", "OrlandoCode", "CountryCode"),
+        new Location("Tokyo", "OrlandoCode", "CountryCode"),
+        new Location("Paris", "OrlandoCode", "CountryCode"),
+        new Location("Sydney", "OrlandoCode", "CountryCode")
+    };
+
+    private const string OrlandoAPICall = $"http://api.openweathermap.org/geo/1.0/direct?q={Locations[0].cityName},{Locations[0].cityCode},{Locations[0].countryCode}&limit={limit}&appid={APIKey}";
+    private const string OrlandoAPICall = $"http://api.openweathermap.org/geo/1.0/direct?q={Locations[0].cityName},{Locations[0].cityCode},{Locations[0].countryCode}&limit={limit}&appid={APIKey}";
+    private const string OrlandoAPICall = $"http://api.openweathermap.org/geo/1.0/direct?q={Locations[0].cityName},{Locations[0].cityCode},{Locations[0].countryCode}&limit={limit}&appid={APIKey}";
+    private const string OrlandoAPICall = $"http://api.openweathermap.org/geo/1.0/direct?q={Locations[0].cityName},{Locations[0].cityCode},{Locations[0].countryCode}&limit={limit}&appid={APIKey}";
+    private const string OrlandoAPICall = $"http://api.openweathermap.org/geo/1.0/direct?q={Locations[0].cityName},{Locations[0].cityCode},{Locations[0].countryCode}&limit={limit}&appid={APIKey}";
+
+    private IEnumerator CallAPI(string url, Action<string> callback) 
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url)) 
+        {    
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError) 
+            {
+                Debug.LogError($"network problem: {request.error}");
+            } 
+            else if (request.result == UnityWebRequest.Result.ProtocolError) 
+            {
+                Debug.LogError($"response error: {request.responseCode}");
+            } 
+            else 
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherJSON(Action<string> callback) 
+    {
+        return CallAPI(OrlandoAPICall, callback);
+    }
 }
